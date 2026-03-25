59a318370ceb3ec92c0d4bd767b91f99b6c3b10c
Matheus Amon <matheus.amon@outlook.com>
2026-03-25 01:19:23 -0300

    💩 simple file for local test

--- src/amon_claw/main.py
+++ src/amon_claw/main.py
@@ -4,52 +4,60 @@ from typing import Annotated, TypedDict
 from langchain_core.messages import AIMessage, AnyMessage, HumanMessage
 from langgraph.graph import END, START, StateGraph
 
+from amon_claw.agents.calculator_agent import MathResult, agent_factory
+
 
 class MessagesState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
     ai_calls: int
+    last_result: float
 
 
 def input_node(state: MessagesState):
-    user_message = input("USER: ")
+    user_message = input('USER: ')
     human_message = HumanMessage(user_message)
-    return {"messages": [human_message]}
+    return {'messages': [human_message]}
 
 
-def calculator_node(state: MessagesState):
-    last_message = state["messages"][-1]
-    result = eval(str(last_message.content))
-    message = f"Aqui está o resultado: {result}"
-    ai_message = AIMessage(message)
+def call_llm_node(state: MessagesState):
+    agent = agent_factory()
+    last_message = str(state['messages'][-1].content)
+    result = agent.run_sync(last_message)
+    message = result.output
+    ai_message = AIMessage(message.explication)
 
-    print(f"AI: {message}")
-    return {"messages": [ai_message], "ai_calls": state["ai_calls"] + 1}
+    print(f'AI: {message}')
+    return {
+        'messages': [ai_message],
+        'ai_calls': state['ai_calls'] + 1,
+        'last_result': message.result,
+    }
 
 
 def should_continue(state: MessagesState):
-    last_message = str(state["messages"][-1].content)
+    last_message = str(state['messages'][-1].content)
 
-    if last_message == "sair":
-        return "END"
+    if last_message == 'sair':
+        return 'END'
 
-    return "calculator_node"
+    return 'call_llm_node'
 
 
 graph = StateGraph(MessagesState)
 
-graph.add_node("input_node", input_node)
-graph.add_node("calculator_node", calculator_node)
+graph.add_node(input_node)
+graph.add_node(call_llm_node)
 
-graph.add_edge(START, "input_node")
-graph.add_edge("calculator_node", "input_node")
+graph.add_edge(START, 'input_node')
+graph.add_edge('call_llm_node', 'input_node')
 
 graph.add_conditional_edges(
-    "input_node", should_continue, {"END": END, "calculator_node": "calculator_node"}
+    'input_node', should_continue, {'END': END, 'call_llm_node': 'call_llm_node'}
 )
 
 app = graph.compile()
 
-if __name__ == "__main__":
-    app.invoke({"messages": [], "ai_calls": 0})
-    with open("grafo.png", "wb") as f:
+if __name__ == '__main__':
+    app.invoke({'messages': [], 'ai_calls': 0, 'last_result': 0.0})
+    with open('grafo.png', 'wb') as f:
         f.write(app.get_graph().draw_mermaid_png())
