cd6ca52f3cb6750de1088983bce6388d9414738a
Matheus Amon <matheus.amon@outlook.com>
2026-03-26 01:31:55 -0300

    ✨ add checkpointer

--- src/amon_claw/main.py
+++ src/amon_claw/main.py
@@ -1,41 +1,48 @@
-import operator
-from typing import Annotated, TypedDict
+import uuid
+from typing import Literal, cast
 
-from langchain_core.messages import AIMessage, AnyMessage, HumanMessage
-from langgraph.graph import END, START, StateGraph
+from langchain_core.messages import AIMessage, HumanMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph import END, START, MessagesState, StateGraph
+from pydantic import EmailStr
+from pydantic_ai.run import AgentRunResult
 
-from amon_claw.agents.calculator_agent import MathResult, agent_factory
+from amon_claw.agents.calculator_agent import agent_factory
+from amon_claw.models.response import MathResult
 
 
-class MessagesState(TypedDict):
-    messages: Annotated[list[AnyMessage], operator.add]
+class AmonClawState(MessagesState):
+    thread_id: uuid.UUID | None
+    user_id: str | int | EmailStr | None
+
     ai_calls: int
     last_result: float
 
 
-def input_node(state: MessagesState):
-    user_message = input('USER: ')
+def input_node(state: AmonClawState):
+    user_message: str = input('USER: ')
     human_message = HumanMessage(user_message)
     return {'messages': [human_message]}
 
 
-def call_llm_node(state: MessagesState):
+def call_llm_node(state: AmonClawState):
     agent = agent_factory()
     last_message = str(state['messages'][-1].content)
-    result = agent.run_sync(last_message)
-    message = result.output
+    result: AgentRunResult[str] = agent.run_sync(user_prompt=last_message)
+    message = cast(MathResult, result.output)
     ai_message = AIMessage(message.explication)
 
-    print(f'AI: {message}')
+    print(f'AI: {message.explication}: {message.result}')
     return {
+        'user_id': 'amon',
         'messages': [ai_message],
         'ai_calls': state['ai_calls'] + 1,
         'last_result': message.result,
     }
 
 
-def should_continue(state: MessagesState):
-    last_message = str(state['messages'][-1].content)
+def should_continue(state: AmonClawState) -> Literal['END', 'call_llm_node']:
+    last_message = state['messages'][-1].content
 
     if last_message == 'sair':
         return 'END'
@@ -43,7 +50,7 @@ def should_continue(state: MessagesState):
     return 'call_llm_node'
 
 
-graph = StateGraph(MessagesState)
+graph = StateGraph(AmonClawState)  # ty: ignore[invalid-argument-type]
 
 graph.add_node(input_node)
 graph.add_node(call_llm_node)
@@ -55,9 +62,19 @@ graph.add_conditional_edges(
     'input_node', should_continue, {'END': END, 'call_llm_node': 'call_llm_node'}
 )
 
-app = graph.compile()
 
 if __name__ == '__main__':
-    app.invoke({'messages': [], 'ai_calls': 0, 'last_result': 0.0})
-    with open('grafo.png', 'wb') as f:
-        f.write(app.get_graph().draw_mermaid_png())
+    with SqliteSaver.from_conn_string('amonclaw_checkpoints.sqlite') as checkpointer:
+        app = graph.compile(checkpointer=checkpointer)
+        app.invoke(
+            {
+                'thread_id': None,
+                'user_id': 'amon',
+                'messages': [],
+                'ai_calls': 0,
+                'last_result': 0.0,
+            },
+            config={'configurable': {'thread_id': uuid.uuid8()}},
+        )
+        with open('grafo.png', 'wb') as f:
+            f.write(app.get_graph().draw_mermaid_png())
